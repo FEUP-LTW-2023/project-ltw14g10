ca9b79bc417fadf70cb4368a52e21515d0209ced
francis802 <franciscosccampos@gmail.com>
2023-04-24 01:20:39 +0100

    Started to do something in main page

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BugBlitz Main</title>
+    <link href="main-style.css" rel="stylesheet">
 </head>
 
 <body>
@@ -13,9 +14,8 @@
         <h1><a href="index.php">BugBlitz</a></h1>
         <h2><a href="index.php">Where your bugs, are fixed in a blitz!</a></h2>
         <div id="signup">
-            <a href="register.php">Register</a>
-            <span id="spacer" style="color: whitesmoke;"> | </span>
-            <a href="login.php">Login</a>
+            <a href="login.php" id="login">Login</a>
+            <a href="register.php" id="register">Register</a>
         </div>
     </header>
 </body>
