2e4de5ff6a2eb9614091e25ef04966612ffe3491
francis802 <franciscosccampos@gmail.com>
2023-04-28 23:47:04 +0100

    Database population started; first classes done

--- database/database.sql
+++ database/database.sql
@@ -72,3 +72,46 @@ CREATE TABLE FAQ (
     FOREIGN KEY (DEPARTMENT_ID) REFERENCES DEPARTMENT(ID)
 );
 
+INSERT INTO DEPARTMENT(ID, DEP_NAME) VALUES
+  (1, 'Sales'),
+  (2, 'Marketing'),
+  (3, 'Finance'),
+  (4, 'Human Resources'),
+  (5, 'Engineering'),
+  (6, 'Product Management'),
+  (7, 'Customer Service'),
+  (8, 'Operations'),
+  (9, 'IT'),
+  (10, 'Legal');
+
+INSERT INTO USER(ID, USERNAME, [PASSWORD], EMAIL, [NAME]) VALUES
+  (1, 'johnsmith', 'password123', 'johnsmith@example.com', 'John Smith'),
+  (2, 'janedoe', 'password456', 'janedoe@example.com', 'Jane Doe'),
+  (3, 'bobdoe', 'password789', 'bobdoe@example.com', 'Bob Doe'),
+  (4, 'sarahjones', 'passwordabc', 'sarahjones@example.com', 'Sarah Jones'),
+  (5, 'davidsmith', 'passworddef', 'davidsmith@example.com', 'David Smith'),
+  (6, 'amandajones', 'passwordghi', 'amandajones@example.com', 'Amanda Jones'),
+  (7, 'peterlee', 'passwordjkl', 'peterlee@example.com', 'Peter Lee'),
+  (8, 'lisajohnson', 'passwordmno', 'lisajohnson@example.com', 'Lisa Johnson'),
+  (9, 'michaelbrown', 'passwordpqr', 'michaelbrown@example.com', 'Michael Brown'),
+  (10, 'karenwilson', 'passwordstu', 'karenwilson@example.com', 'Karen Wilson');
+
+  INSERT INTO CLIENT(USER_ID) VALUES
+  (1),
+  (2),
+  (3),
+  (4),
+  (5),
+  (6),
+  (7),
+  (8),
+  (9),
+  (10);
+
+  INSERT INTO AGENT(USER_ID) VALUES
+  (1,1),
+  (2,2),
+  (3,3);
+
+  INSERT INTO ADMIN(USER_ID) VALUES
+  (1);
